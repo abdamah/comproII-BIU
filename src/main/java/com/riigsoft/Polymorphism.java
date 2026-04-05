dc8d5bffec207fb393e1a66ba9bc7d43be463781
abdamah <abdamoh1@gmail.com>
2026-04-05 21:28:41 +0300

    Polymorphism

--- src/main/java/com/riigsoft/Polymorphism.java
+++ src/main/java/com/riigsoft/Polymorphism.java
@@ -1,11 +1,20 @@
 package com.riigsoft;
 
+/* Poly-> many : morphism: many
+    polymorphism : many forms
+    types of polymorphism
+    1. Overriding -> parent -> child methods
+    2. Overloading -> a method with same name
+    but different signature in the same class.
+*/
 public class Polymorphism {
 
+    // method with 2 integer params
     public void sum(int a, int b){
         System.out.println(a + b);
     }
 
+    // method with 3 integer params (overloaded)
     public void sum (int a, int b, int c){
         System.out.println(a -b);
     }
